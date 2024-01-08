cbf3c49486beba3c9c7a1b8fcde2d0beb27db851
whiletrue <baptiste.isenbrandt@gmail.com>
2024-01-08 21:33:56 +0100

    Improve perf

--- 1brc/Program.cs
+++ 1brc/Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.IO.MemoryMappedFiles;
 using System.Numerics;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace _1brc;
@@ -16,8 +18,6 @@ class Program
 
         internal readonly long _fileLength;
 
-        internal ConcurrentDictionary<string, string> map = new();
-        
         public Challenge(string filename)
         {
             FileInfo fileInfo = new FileInfo(filename);
@@ -27,11 +27,11 @@ class Program
             _viewStream = mappedFile.CreateViewStream();
         }
 
-        unsafe List<(long, long)> split(string filename)
+        unsafe List<(long, long)> split()
         {
             List<(long, long)> chunkOffsets = new List<(long, long)>();
 
-            long maxChunkSize = _viewStream.Length / Environment.ProcessorCount;
+            long maxChunkSize = _viewStream.Length / 8;
 
             long length = _viewStream.Length;
             byte* pointer = null;
@@ -67,64 +67,102 @@ class Program
 
             return chunkOffsets;
         }
-
-
-        unsafe public void Run()
+        
+        static int GetHashCode(ReadOnlySpan<byte> span)
         {
-            var chunks = split("./measurements.txt");
+            int hashCode = 0;
+            for (int i = 0; i < span.Length; i++)
+                hashCode = 31 * hashCode + span[i];
+            return hashCode;
+        }
+
+        unsafe Dictionary<int, string> Compute((long, long) chunk, byte* pointer)
+        {
+            
+            long startPosition = chunk.Item1;
+            long chunkSize = chunk.Item2;
+
+            ReadOnlySpan<byte> chunkSpan = new ReadOnlySpan<byte>(pointer + startPosition, (int)chunkSize);
+
+            int start = 0;
+            int end = 0;
+
+            Dictionary<int, string> localMap = new();
+            
+            while (end < chunkSpan.Length)
+            {
+                while (end < chunkSpan.Length && chunkSpan[end] != '\n' && chunkSpan[end] != '\r')
+                {
+                    end++;
+                }
+
+                var line = chunkSpan.Slice(start, end - start);
+                
+                ref byte spanRef = ref MemoryMarshal.GetReference(line);
+                
+                int separator = -1;
+                for (int i = 4; i <= 7; i++)
+                {
+                    if (Unsafe.Add(ref spanRef, line.Length - i) == (byte)';')
+                    {
+                        separator = line.Length - i;
+                        break;
+                    }
+                }
+                
+                var city = line.Slice(0, separator).ToArray();
+                var temperature = line.Slice(separator + 1, line.Length - separator - 1);
+                
+                // Convert the input string to a byte array and compute the hash.
+                var key = GetHashCode(city);
+                
+                ref var refVal = ref CollectionsMarshal.GetValueRefOrAddDefault(localMap, key, out bool exit);
+                if (!exit)
+                {
+                    refVal = Encoding.UTF8.GetString(temperature);
+                }
+
+                // Move to the next line
+                if (end < chunkSpan.Length && (chunkSpan[end] == '\n' || chunkSpan[end] == '\r'))
+                {
+                    // Move past the newline character(s)
+                    if (end + 1 < chunkSpan.Length && (chunkSpan[end] == '\n' && chunkSpan[end + 1] == '\r'))
+                    {
+                        end += 2;
+                    }
+                    else
+                    {
+                        end++;
+                    }
+                }
+
+                start = end;
+            }
+
+            return localMap;
+
+        }
+
+        public unsafe void Run()
+        {
+            var chunks = split();
+
+            var tasks = new List<Task>(chunks.Count);
 
             byte* pointer = null;
 
             _viewStream.SafeMemoryMappedViewHandle.AcquirePointer(ref pointer);
-            
-            chunks
-#if !DEBUG
-                .AsParallel()
-#endif
-                .Select(chunk =>
-                {
-                    long startPosition = chunk.Item1;
-                    long chunkSize = chunk.Item2;
 
-                    ReadOnlySpan<byte> chunkSpan = new ReadOnlySpan<byte>(pointer + startPosition, (int)chunkSize);
+            foreach (var chunk in chunks)
+            {
+                var task = Task.Run(() => Compute(chunk, pointer));
+                tasks.Add(task);
+            }
 
-                    int start = 0;
-                    int end = 0;
-
-                    while (end < chunkSpan.Length)
-                    {
-                        while (end < chunkSpan.Length && chunkSpan[end] != '\n' && chunkSpan[end] != '\r')
-                        {
-                            end++;
-                        }
-                        var line = chunkSpan.Slice(start, end - start);
-
-                        var separator = line.IndexOf((byte)';');
-
-                        var city = line.Slice(0, separator);
-                        var temperature = line.Slice(separator, line.Length - separator);
-                        
-                        
-                        // Move to the next line
-                        if (end < chunkSpan.Length && (chunkSpan[end] == '\n' || chunkSpan[end] == '\r'))
-                        {
-                            // Move past the newline character(s)
-                            if (end + 1 < chunkSpan.Length && (chunkSpan[end] == '\n' && chunkSpan[end + 1] == '\r'))
-                            {
-                                end += 2;
-                            }
-                            else
-                            {
-                                end++;
-                            }
-                        }
-
-                        start = end;
-                    }
-
-                    return chunk;
-                }).ToList();
-            
+            Task
+                .WhenAll(tasks)
+                .GetAwaiter()
+                .GetResult();
             _viewStream.SafeMemoryMappedViewHandle.ReleasePointer();
         }
     }
